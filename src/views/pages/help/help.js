72364ab6ceedbe78654af60074f5125b9b46c967
dengbf <dengbf@aliyun.com>
2023-06-08 10:38:54 +0800

    commit

--- src/views/pages/help/help.js
+++ src/views/pages/help/help.js
@@ -7,7 +7,7 @@ import VueI18n from 'vue-i18n';
 import CompareUtil from '@/resources/assets/js/compareUtil.js';
 import md5 from 'js-md5';
 import api from '@/resources/api/index/index.js';
-
+import '@/resources/base.js';
 import {initRouter, initI18n} from '@/resources/init.js';
 
 MODULEID = 'help';
@@ -16,8 +16,6 @@ MENULIST = indexRouter;
 Vue.use(VueRouter);
 Vue.use(VueI18n);
 Vue.use(CompareUtil);//必须要在use router之后执行
-
-import '@/resources/base.js';
 
 let router = initRouter(VueRouter, store);//路由拦截处理
 let i18n = initI18n(VueI18n, {});//语言包配置
