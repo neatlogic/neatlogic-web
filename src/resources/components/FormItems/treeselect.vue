cb42095f7b9523408237e36c24bc7de7521a750a
dengbf <dengbf@aliyun.com>
2023-07-20 14:44:21 +0800

    - [修复] 选择表单组件作为参数值的数据补充和字段调整  - [关联] 引用表单组件作为条件，需要调整为新表单数据 http://192.168.0.96:8090/demo/rdm.html#/task-list/doing

---
+++ src/resources/components/FormItems/treeselect.vue
@@ -0,0 +1,91 @@
+<template>
+  <TsFormTree
+    ref="formitem"
+    :value="value"
+    v-bind="getSetting"
+    :readonly="readonly"
+    transfer
+    @change="updateval"
+  >
+    ></TsFormTree>
+</template>
+
+<script>
+import TsFormTree from '@/resources/plugins/TsForm/TsFormTree';
+export default {
+  name: '',
+  components: {
+    TsFormTree
+  },
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+  props: {
+    config: Object,
+    value: [String, Boolean, Array],
+    readonly: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {};
+  },
+
+  beforeCreate() {},
+
+  created() {},
+
+  beforeMount() {
+    this.val = this.value;
+  },
+
+  mounted() {},
+
+  beforeUpdate() {},
+
+  updated() {},
+
+  activated() {},
+
+  deactivated() {},
+
+  beforeDestroy() {},
+
+  destroyed() {},
+
+  methods: {
+    updateval(val) {
+      this.$emit('change', val);
+    }
+  },
+
+  filter: {},
+
+  computed: {
+    getSetting() {
+      let setting = this.config.config;
+      let config = {
+        url: setting.url,
+        desc: '',
+        params: {isActive: 1},
+        valueName: setting.valueName || 'uuid',
+        textName: setting.textName || 'name',
+        childrenKey: setting.childrenKey || 'children',
+        validateList: [],
+        placeholder: this.config.placeholder,
+        clearable: true,
+        showPath: true,
+        sperateText: '>',
+        border: 'border',
+        readonlyClass: setting.readonlyClass
+      };
+      return config;
+    }
+  },
+
+  watch: {}
+};
+</script>
+<style lang="less" scoped></style>
