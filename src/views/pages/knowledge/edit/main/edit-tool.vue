085cc3bc6f5fad3f93bfdf47c347ae288356b58d
dengbf <dengbf@aliyun.com>
2023-08-15 10:35:56 +0800

    [修复] 知识编辑，查找替换报错  - [关联] #[957961881485312]知识编辑-查找替换时，在查找和替换输入框敲回车键会导致整个输入框换行 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/957961881485312

--- src/views/pages/knowledge/edit/main/edit-tool.vue
+++ src/views/pages/knowledge/edit/main/edit-tool.vue
@@ -400,7 +400,7 @@ export default {
         // //当没有选中的数据，利用焦点元素
         let range = editorUtils.comGetCursor();
         let activeRange = {startContainer: range.startContainer, startOffset: range.startOffset, commonAncestorContainer: range.commonAncestorContainer};
-        $target.children.forEach(child => {
+        Array.from($target.children).forEach(child => {
           let $mark = child.querySelectorAll('mark');
           if ($mark && $mark.length > 0) {
             if ($focusTarget == child) {
