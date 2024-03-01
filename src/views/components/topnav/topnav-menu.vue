7bb98e7270108ad08f950bf1c0b81732663ba223
dengbf <dengbf@aliyun.com>
2024-03-01 10:37:16 +0800

    - [修复] 保存数据窗口：“不保存”按钮要点击两次才有效果    - [关联] #[1083909826117632]工单上报-在上报页面，点击it服务跳转出去，出现三层保存提示弹窗 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1083909826117632

--- src/views/components/topnav/topnav-menu.vue
+++ src/views/components/topnav/topnav-menu.vue
@@ -85,7 +85,19 @@ export default {
       }
       this.$route.meta.clearHistory = true;
       if (module === MODULEID) {
-        this.$router.push(path);
+        let defaultPage = '';
+        try {
+          const moduleList = JSON.parse(localStorage.getItem('moduleList'));
+          defaultPage =
+          moduleList.find(module => {
+            return module.moduleId === MODULEID;
+          }).defaultPage || '/welcome';
+        } catch {
+          defaultPage = '/welcome';
+        }
+        this.$router.push({
+          path: defaultPage
+        });
       } else {
         let that = this.$root.$children[0] ? this.$root.$children[0].$refs.root : null;//获取router-view 的vue 对象
         this.$utils.gotoHref(`${HOME}/${module}.html#${path}`, that);
