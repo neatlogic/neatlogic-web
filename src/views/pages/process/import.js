d6684cbc1395fe7e202c4112214960b9028818cc
yaojn <everast@aliyun.com>
2024-06-11 19:51:40 +0800

    - [功能]前端分派器抽象分离   - [关联]#[1166715679899648]前端分派器抽象分离 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1166715679899648

---
+++ src/views/pages/process/import.js
@@ -0,0 +1,12 @@
+import dispatcherValid from '@/views/pages/process/flow/flowedit/components/nodesetting/dispatcher/dispatcher-valid.js';
+// 流程分派器
+let FLOW_DISPATCHER = {
+  DISPATCHER_COMPONENT: {
+    // 分派器组件
+  },
+  DISPATCHER_VALID: {
+    // 分派器校验
+    ...dispatcherValid
+  }
+};
+export { FLOW_DISPATCHER };
