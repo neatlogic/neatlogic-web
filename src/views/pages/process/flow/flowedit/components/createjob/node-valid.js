3df220cec4acf4df60589fcc703a80266284bbee
dengbf <dengbf@aliyun.com>
2024-06-28 12:22:03 +0800

    校验

--- src/views/pages/process/flow/flowedit/components/createjob/node-valid.js
+++ src/views/pages/process/flow/flowedit/components/createjob/node-valid.js
@@ -1,23 +1,21 @@
 import { $t } from '@/resources/init.js';
-const nodeValidConfig = {
-  createjob(nodeConfig, d, that) {
-    //节点
-    let validList = [];
-    let nodeData = nodeConfig.stepConfig || {};
-    let createJobConfig = nodeData.createJobConfig || {};
-    if (!createJobConfig.failPolicy) {
-      validList.push({
-        name: $t('form.validate.required', { target: $t('page.failurestrategy') }),
-        href: '#createJobConfig'
-      });
-    }
-    if (that.$utils.isEmpty(createJobConfig.configList)) {
-      validList.push({
-        name: $t('form.validate.leastonetarget', { target: $t('term.autoexec.job') }),
-        href: '#createJobConfig'
-      });
-    }
-    return validList;
+export const createjobNodeValid = (nodeConfig, d, that) => {
+  //节点
+  let validList = [];
+  let nodeData = nodeConfig.stepConfig || {};
+  let createJobConfig = nodeData.createJobConfig || {};
+  if (!createJobConfig.failPolicy) {
+    validList.push({
+      name: $t('form.validate.required', { target: $t('page.failurestrategy') }),
+      href: '#createJobConfig'
+    });
   }
+  if (that.$utils.isEmpty(createJobConfig.configList)) {
+    validList.push({
+      name: $t('form.validate.leastonetarget', { target: $t('term.autoexec.job') }),
+      href: '#createJobConfig'
+    });
+  }
+  return validList;
 };
-export { nodeValidConfig };
+
