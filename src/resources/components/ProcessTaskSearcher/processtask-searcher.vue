41ebc1bf89a72075d84acb135a48e195c3745622
lvzk <897706680@qq.com>
2024-07-23 19:04:14 +0800

    [功能]工单中心标题改成like搜索 #[1207326827053056]工单中心标题改成like搜索 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1207326827053056

--- src/resources/components/ProcessTaskSearcher/processtask-searcher.vue
+++ src/resources/components/ProcessTaskSearcher/processtask-searcher.vue
@@ -66,6 +66,10 @@
                     {{ $t('form.placeholder.searchtarget',{target:$t('term.process.reportcontent')}) }}：
                     <b>{{ keyword }}</b>
                   </DropdownItem>
+                  <DropdownItem v-if="keyword" @click.native="addKeywordCondition({ name: 'title', text: $t('page.title'), value: keyword })">
+                    {{ $t('form.placeholder.searchtarget',{target:$t('page.title')}) }}：
+                    <b>{{ keyword }}</b>
+                  </DropdownItem>
                   <div v-for="(k, index) in forecastKeyworkList" :key="index">
                     <div v-if="k.dataList && k.dataList.length > 0">
                       <div>
