37fe3e62c989b4960f73e720d6de73979a9ec691
chenqw <zenkiray@live.com>
2024-08-13 16:17:27 +0800

    -[功能]配置项模型增加多选集合类型字段

---
+++ src/views/pages/cmdb/ci/attrhandler/search/set-attr.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <TsFormCheckbox
+      ref="handler"
+      :value="value"
+      :dataList="dataList"
+      @change="setData"
+    ></TsFormCheckbox>
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  components: {
+    TsFormCheckbox: () => import('@/resources/plugins/TsForm/TsFormCheckbox')
+  },
+  props: {
+    attrData: { type: Object },
+    valueList: { type: Array }
+  },
+  data() {
+    return {};
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  activated() {},
+  deactivated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {
+    setData(val) {
+      this.$emit('setData', val);
+      this.$emit('setValue', val);
+    },
+    valid() {
+      //验证数据合法性
+      const handler = this.$refs['handler'];
+      return handler.valid();
+    }
+  },
+  filter: {},
+  computed: {
+    dataList: function() {
+      const dataList = [];
+      if (this.attrData.config && this.attrData.config.members) {
+        this.attrData.config.members.forEach(element => {
+          if (element && element.trim()) {
+            dataList.push({ text: element.trim(), value: element.trim() });
+          }
+        });
+      }
+      return dataList;
+    },
+    value: function() {
+      if (this.valueList && this.valueList.length > 0) {
+        return this.valueList;
+      }
+      return '';
+    }
+  },
+  watch: {}
+};
+</script>
+<style lang="less"></style>
