705c721a1f93877d7951abc42f8658b64626e419
dengbf <dengbf@aliyun.com>
2024-08-19 17:24:17 +0800

    表单-联动规则

---
+++ src/resources/plugins/TsSheet/form/reaction/index.js
@@ -0,0 +1,23 @@
+import { default as mask } from './mask.js';
+import { default as hide } from './hide.js';
+import { default as display } from './display.js';
+import { default as readonly } from './readonly.js';
+import { default as disable } from './disable.js';
+import { default as setvalue } from './setvalue.js';
+import { default as filter } from './filter.js';
+import { default as emit } from './emit.js';
+import { default as required } from './required.js';
+import { default as clearValue } from './clearValue.js';
+
+export const REACTION = {
+  mask: mask,
+  hide: hide,
+  display: display,
+  readonly: readonly,
+  disable: disable,
+  setvalue: setvalue,
+  filter: filter,
+  emit: emit,
+  required: required,
+  clearValue: clearValue
+};
