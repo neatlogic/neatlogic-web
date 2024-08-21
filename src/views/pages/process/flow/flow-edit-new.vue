30f5d737a6591459d052b7916b9eda2431ed6af4
dengbf <dengbf@aliyun.com>
2024-08-21 10:56:07 +0800

    commit

--- src/views/pages/process/flow/flow-edit-new.vue
+++ src/views/pages/process/flow/flow-edit-new.vue
@@ -890,10 +890,10 @@ export default {
       //从流程设置切走的时候，保存表单id
       if (this.$refs.flowSetting) {
         const process = this.$refs.flowSetting ? this.$refs.flowSetting.getJsonValue() : {};
-        // const formConfig = this.$refs.flowSetting.getJsonValue().formConfig ? this.$refs.flowSetting.getJsonValue().formConfig : {};
+        const formConfig = process.formConfig ? process.formConfig : {};
         //console.log(JSON.stringify(formConfig, null, 2));
         this.$set(this.flowData.process, 'processConfig', process.processConfig);
-        this.flowData.process.formConfig.uuid = process.formConfig?.uuid;
+        this.flowData.process.formConfig.uuid = formConfig.uuid;
       }
     },
     /**
