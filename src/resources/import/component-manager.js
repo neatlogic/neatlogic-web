b439053ed22cf4e89c63b376165697d0e95262f9
lvzk <897706680@qq.com>
2024-09-04 11:35:01 +0800

    [功能]扩展部署配置

--- src/resources/import/component-manager.js
+++ src/resources/import/component-manager.js
@@ -5,7 +5,7 @@
  * 获取方法：getCategoryComponent() // Category来自categoryList，首字母大写
  */
 class ComponentManager {
-  static categoryList = ['timeLine', 'taskDetail', 'stepLog', 'flowNode', 'flowElement', 'flowNodeValid', 'flowLineValid', 'dispatcher', 'dispatcherValid', 'workCenterColumn', 'formDefine', 'formConfig', 'formComponent'];
+  static categoryList = ['timeLine', 'taskDetail', 'stepLog', 'flowNode', 'flowElement', 'flowNodeValid', 'flowLineValid', 'dispatcher', 'dispatcherValid', 'workCenterColumn', 'formDefine', 'formConfig', 'formComponent', 'deployAppConfigEnvTab'];
   static categoryConfig = {};
   static generateMethods() {
     this.categoryList.forEach(category => {
