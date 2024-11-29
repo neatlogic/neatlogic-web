2cadd18646f82fcde839d997df71dfa7db7c111e
yaojn <yaojn@techsure.com.cn>
2024-11-29 10:49:28 +0800

    - [修复]工单处理-自动化服务组件的数据保存成功，但是没有回显   - [关联]#[1300053887516672]工单处理-自动化服务组件的数据保存成功，但是没有回显 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1300053887516672

--- src/views/pages/autoexec/quick-service/service-catalog/no-form-service-edit.vue
+++ src/views/pages/autoexec/quick-service/service-catalog/no-form-service-edit.vue
@@ -297,15 +297,16 @@ export default {
   methods: {
     defaultValue() {
       if (this.source == 'form') {
-        for (let key in this.serviceData) {
+        let deepCloneData = this.$utils.deepClone(this.serviceData);
+        for (let key in deepCloneData) {
           if (this.hasOwnProperty(key)) {
-            this[key] = this.serviceData[key];
+            this[key] = deepCloneData[key];
           } else if (key == 'name') {
-            this.jobName = this.serviceData[key];
+            this.jobName = deepCloneData[key];
           }
         }
         if (!this.$utils.isEmpty(this.runtimeParamList)) {
-          this.initConfig(this.serviceData['runtimeParamMap']);
+          this.initConfig(deepCloneData['runtimeParamMap']);
         }
       }
     },
@@ -358,17 +359,22 @@ export default {
     initData() {
       // 初始化
       this.defaultInitData();
-      if (!this.$utils.isEmpty(this.defaultData)) {
-        if (this.defaultData.config && !this.$utils.isEmpty(this.defaultData.config)) {
-          for (let key in this.defaultData.config) {
-            if (key && this.defaultData.config[key] && this.hasServiceValue.hasOwnProperty(key) && (this.defaultData.config[key]['mappingMode'] == 'notsetup')) {
+      let defaultData = this.$utils.deepClone(this.defaultData);
+      let serviceData = this.$utils.deepClone(this.serviceData);
+      let {config = {}, combopId, combopName} = defaultData || {};
+      let {executeNodeConfig = {}} = config || {};
+      let {value = ''} = executeNodeConfig || {};
+      if (!this.$utils.isEmpty(defaultData)) {
+        if (config && !this.$utils.isEmpty(config)) {
+          for (let key in config) {
+            if (key && config[key] && this.hasServiceValue.hasOwnProperty(key) && (config[key]['mappingMode'] == 'notsetup')) {
               // 映射关系为notsetup时，需要把对应的组件显示出来
               this.$set(this.hasServiceValue, [key], true);
             }
           }
-          if (this.defaultData.config && !this.$utils.isEmpty(this.defaultData.config.runtimeParamList)) {
+          if (config && !this.$utils.isEmpty(config.runtimeParamList)) {
             // 作业参数映射关系为不设置时，需要把对应作业参数显示出来
-            this.defaultData.config.runtimeParamList.forEach((item) => {
+            config.runtimeParamList.forEach((item) => {
               if (item && item.mappingMode == 'notsetup') {
                 this.$set(this.hasServiceValue, 'runtimeParamList', true);
               }
@@ -378,13 +384,10 @@ export default {
             });
           }
         }
-        let {config = {}} = this.defaultData || {};
-        let {executeNodeConfig = {}} = config || {};
-        let {value = ''} = executeNodeConfig || {};
-        this.filterSearchValue = this.serviceData && !this.$utils.isEmpty(this.serviceData.executeNodeConfig) ? this.serviceData.executeNodeConfig : !this.$utils.isEmpty(value) ? value : {}; // 执行目标值回显
-        this.combopId = this.defaultData.combopId || null;
-        if (!this.$utils.isEmpty(this.defaultData) && this.defaultData.combopName) {
-          this.jobName = this.defaultData.combopName;
+        this.filterSearchValue = serviceData && !this.$utils.isEmpty(serviceData.executeNodeConfig) ? serviceData.executeNodeConfig : !this.$utils.isEmpty(value) ? value : {}; // 执行目标值回显
+        this.combopId = combopId || null;
+        if (combopName) {
+          this.jobName = combopName;
         }
         if (this.combopId) {
           return this.getCombopDetail();
@@ -574,20 +577,7 @@ export default {
       return this.border == 'border' ? 'radius-lg bg-op padding mt-nm' : '';
     }
   },
-  watch: {
-    defaultData: {
-      handler() {
-        this.initData();
-      },
-      deep: true
-    },
-    serviceData: {
-      handler() {
-        this.defaultValue();
-      },
-      deep: true
-    }
-  }
+  watch: {}
 };
 </script>
 <style lang="less" scoped>
