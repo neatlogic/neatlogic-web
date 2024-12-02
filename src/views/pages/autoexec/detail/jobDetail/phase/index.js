616df961afbd9f20ac93e929c43dbd9e80d91523
lvzk <897706680@qq.com>
2024-12-02 08:50:36 +0800

    [功能]新增runner状态

--- src/views/pages/autoexec/detail/jobDetail/phase/index.js
+++ src/views/pages/autoexec/detail/jobDetail/phase/index.js
@@ -1,11 +1,13 @@
 import targetContent from './node-list.vue';
 import runner_targetContent from './node-list.vue';
 import runnerContent from './runner-content.vue';
+import nativeContent from './runner-content.vue';
 import sqlfileContent from './sqlfile-content.vue';
 
 export default {
   runnerContent,
   targetContent,
   runner_targetContent,
-  sqlfileContent
+  sqlfileContent,
+  nativeContent
 };
