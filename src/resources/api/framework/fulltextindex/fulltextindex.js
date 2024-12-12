f05303a29e2628fff1ec4141d159b3998279ae8a
chenqw <gdnpc@163.com>
2024-12-12 15:34:46 +0800

    adjust

--- src/resources/api/framework/fulltextindex/fulltextindex.js
+++ src/resources/api/framework/fulltextindex/fulltextindex.js
@@ -2,7 +2,7 @@ import axios from '../../http';
 
 const fulltextindex = {
   getFullTextIndexRebuildAuditList(params) {
-    return axios.get('/api/rest/fulltextindex/rebuildaudit/list', params);
+    return axios.post('/api/rest/fulltextindex/rebuildaudit/list', params);
   },
   rebuildFullTextIndex(params) {
     return axios.post('/api/rest/fulltextindex/rebuild', params);
--- src/views/pages/framework/fulltextindex/fulltextindex-manage.vue
+++ src/views/pages/framework/fulltextindex/fulltextindex-manage.vue
@@ -2,37 +2,78 @@
   <div class="padding-t">
     <TsContain :hideHeader="true">
       <div slot="content">
-        <TsTable v-bind="fullTextIndexRebuildAuditData">
-          <template v-slot:error="{ row }">
-            <Poptip
-              v-if="row.error"
-              trigger="hover"
-              :title="$t('page.exception')"
-              word-wrap
-              width="400"
-              :transfer="true"
-              :content="row.error"
-              placement="left"
-            >
-              <span class="text-error tsfont-warning-s"></span>
-            </Poptip>
-          </template>
-          <template v-slot:statusText="{ row }">
-            <Progress v-if="row.status==='done'" :percent="100">
-              <span>{{ row.statusText }}</span>
-            </Progress>
-            <Progress v-else-if="row.status==='doing'" :percent="99" status="active">
-              <span>{{ row.statusText }}</span>
-            </Progress>
-          </template>
-          <template v-slot:action="{ row }">
-            <div class="tstable-action">
-              <ul class="tstable-action-ul">
-                <li :class="row.status == 'doing'?'disabled':''" class="tsfont-restart" @click="rebuildIndex(row)">{{ $t('page.rebuildindex') }}</li>
-              </ul>
-            </div>
-          </template>
-        </TsTable>
+        <Tabs v-model="currentTab">
+          <TabPane label="数据库" name="database">
+            <TsTable :tbodyList="fullTextIndexRebuildAuditData['database']" :theadList="theadList">
+              <template v-slot:error="{ row }">
+                <Poptip
+                  v-if="row.error"
+                  trigger="hover"
+                  :title="$t('page.exception')"
+                  word-wrap
+                  width="400"
+                  :transfer="true"
+                  :content="row.error"
+                  placement="left"
+                >
+                  <span class="text-error tsfont-warning-s"></span>
+                </Poptip>
+              </template>
+              <template v-slot:statusText="{ row }">
+                <span v-if="row.status === 'done'" class="text-success">{{ row.statusText }}</span>
+                <Progress
+                  v-else-if="row.status === 'doing'"
+                  :percent="99"
+                  status="active"
+                  style="width:110px"
+                ><span></span>
+                </Progress>
+              </template>
+              <template v-slot:action="{ row }">
+                <div class="tstable-action">
+                  <ul class="tstable-action-ul">
+                    <li :class="row.status == 'doing' ? 'disabled' : ''" class="tsfont-restart" @click="rebuildIndex(row)">{{ $t('page.rebuildindex') }}</li>
+                  </ul>
+                </div>
+              </template>
+            </TsTable>
+          </TabPane>
+          <TabPane label="Elasticsearch" name="elasticsearch">
+            <TsTable :tbodyList="fullTextIndexRebuildAuditData['elasticsearch']" :theadList="theadList">
+              <template v-slot:error="{ row }">
+                <Poptip
+                  v-if="row.error"
+                  trigger="hover"
+                  :title="$t('page.exception')"
+                  word-wrap
+                  width="400"
+                  :transfer="true"
+                  :content="row.error"
+                  placement="left"
+                >
+                  <span class="text-error tsfont-warning-s"></span>
+                </Poptip>
+              </template>
+              <template v-slot:statusText="{ row }">
+                <span v-if="row.status === 'done'" class="text-success">{{ row.statusText }}</span>
+                <Progress
+                  v-else-if="row.status === 'doing'"
+                  :percent="99"
+                  status="active"
+                  style="width:110px"
+                ><span></span>
+                </Progress>
+              </template>
+              <template v-slot:action="{ row }">
+                <div class="tstable-action">
+                  <ul class="tstable-action-ul">
+                    <li :class="row.status == 'doing' ? 'disabled' : ''" class="tsfont-restart" @click="rebuildIndex(row)">{{ $t('page.rebuildindex') }}</li>
+                  </ul>
+                </div>
+              </template>
+            </TsTable>
+          </TabPane>
+        </Tabs>
         <FullIndexRebuildDialog v-if="isRebuildDialogShow" :audit="currentAudit" @close="closeRebuildDialog"></FullIndexRebuildDialog>
       </div>
     </TsContain>
@@ -48,38 +89,51 @@ export default {
   props: {},
   data() {
     return {
+      currentTab: 'database',
       isRebuildDialogShow: false,
       currentAudit: null,
+      theadList: [
+        {
+          key: 'typeName',
+          title: this.$t('page.type')
+        },
+        { key: 'statusText', title: this.$t('page.status') },
+        { key: 'indexCount', title: this.$t('page.indexcount') },
+        { key: 'startTime', title: this.$t('page.rebuildstartime'), type: 'time' },
+        { key: 'endTime', title: this.$t('page.rebuildendtime'), type: 'time' },
+        { key: 'error', title: this.$t('page.exception') },
+        { key: 'action' }
+      ],
       fullTextIndexRebuildAuditData: {
-        theadList: [{key: 'typeName', title: this.$t('page.type')},
-          {key: 'statusText', title: this.$t('page.status')},
-          {key: 'indexCount', title: this.$t('page.indexcount')},
-          {key: 'startTime', title: this.$t('page.rebuildstartime'), type: 'time'},
-          {key: 'endTime', title: this.$t('page.rebuildendtime'), type: 'time'},
-          {key: 'error', title: this.$t('page.exception')},
-          {key: 'action'}]
+        database: [],
+        elasticsearch: []
       },
-      doingIdList: [], //正在创建的type列表
-      timer: null//刷新定时器
+      doingIdList: {
+        database: [],
+        elasticsearch: []
+      }, //正在创建的type列表
+      timer: null //刷新定时器
     };
   },
   beforeCreate() {},
   created() {},
   beforeMount() {},
   mounted() {
-    this.getFullTextIndexRebuildAuditList();
+    //this.getFullTextIndexRebuildAuditList('database');
   },
   beforeUpdate() {},
   updated() {},
   activated() {},
   deactivated() {},
-  beforeDestroy() {},
+  beforeDestroy() {
+    this.clearTimer();
+  },
   destroyed() {},
   methods: {
     closeRebuildDialog(needRefresh) {
       this.isRebuildDialogShow = false;
       if (needRefresh) {
-        this.doingIdList.push(this.currentAudit.type);
+        this.doingIdList[this.currentAudit.handler].push(this.currentAudit.type);
         this.checkAuditStatusInterval();
       }
     },
@@ -87,15 +141,15 @@ export default {
       this.currentAudit = audit;
       this.isRebuildDialogShow = true;
     },
-    getFullTextIndexRebuildAuditList() {
-      this.$api.framework.fulltextindex.getFullTextIndexRebuildAuditList().then(res => {
-        this.$set(this.fullTextIndexRebuildAuditData, 'tbodyList', res.Return);
+    getFullTextIndexRebuildAuditList(handler) {
+      this.$api.framework.fulltextindex.getFullTextIndexRebuildAuditList({ handler: handler }).then(res => {
+        this.fullTextIndexRebuildAuditData[handler] = res.Return;
         res.Return.forEach(element => {
-          if (element.status == 'doing') {
-            this.doingIdList.push(element.type);
+          if (element.status === 'doing') {
+            this.doingIdList[handler].push(element.type);
           }
         });
-        if (this.doingIdList.length > 0) {
+        if (this.doingIdList[handler].length > 0) {
           this.timer = setTimeout(() => {
             this.checkAuditStatusInterval();
           }, 3000);
@@ -104,31 +158,36 @@ export default {
     },
     checkAuditStatusInterval() {
       this.clearTimer();
-      if (this.doingIdList && this.doingIdList.length > 0) {
-        this.$api.framework.fulltextindex.getFullTextIndexRebuildAuditList({typeList: this.doingIdList}).then(res => {
-          this.doingIdList = [];
-          if (res.Return && res.Return.length > 0) {
-            res.Return.forEach(element => {
-              const oldElement = this.fullTextIndexRebuildAuditData.tbodyList.find(a => a.type == element.type);
-              if (element.status == 'doing') {
-                this.doingIdList.push(element.type);
-              }
-              if (oldElement) {
-                this.$set(oldElement, 'status', element.status);
-                this.$set(oldElement, 'statusText', element.statusText);
-                this.$set(oldElement, 'indexCount', element.indexCount);
-                this.$set(oldElement, 'startTime', element.startTime);
-                this.$set(oldElement, 'endTime', element.endTime);
-                this.$set(oldElement, 'error', element.error);
-              }
-            });
-          }
-          if (this.doingIdList.length > 0) {
-            this.timer = setTimeout(() => {
-              this.checkAuditStatusInterval();
-            }, 3000);
-          }
-        });
+      if (this.doingIdList[this.currentTab] && this.doingIdList[this.currentTab].length > 0) {
+        this.$api.framework.fulltextindex
+          .getFullTextIndexRebuildAuditList({
+            handler: this.currentTab,
+            typeList: this.doingIdList[this.currentTab]
+          })
+          .then(res => {
+            this.doingIdList[this.currentTab] = [];
+            if (res.Return && res.Return.length > 0) {
+              res.Return.forEach(element => {
+                const oldElement = this.fullTextIndexRebuildAuditData[this.currentTab].find(a => a.type == element.type);
+                if (element.status === 'doing') {
+                  this.doingIdList[element.handler].push(element.type);
+                }
+                if (oldElement) {
+                  this.$set(oldElement, 'status', element.status);
+                  this.$set(oldElement, 'statusText', element.statusText);
+                  this.$set(oldElement, 'indexCount', element.indexCount);
+                  this.$set(oldElement, 'startTime', element.startTime);
+                  this.$set(oldElement, 'endTime', element.endTime);
+                  this.$set(oldElement, 'error', element.error);
+                }
+              });
+            }
+            if (this.doingIdList[this.currentTab].length > 0) {
+              this.timer = setTimeout(() => {
+                this.checkAuditStatusInterval();
+              }, 3000);
+            }
+          });
       }
     },
     clearTimer() {
@@ -140,8 +199,14 @@ export default {
   },
   filter: {},
   computed: {},
-  watch: {}
+  watch: {
+    currentTab: {
+      handler(val) {
+        this.getFullTextIndexRebuildAuditList(val);
+      },
+      immediate: true
+    }
+  }
 };
 </script>
-<style lang="less">
-</style>
+<style lang="less"></style>
