2821614579288105a0167481cfa92ee249ff20e5
yaojn <yaojn@techsure.com.cn>
2024-12-27 12:04:16 +0800

    - [功能]前端定制模块适配新版本neatlogic   - [关联]#[1299329581547520]前端定制模块适配新版本neatlogic http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1299329581547520

--- src/views/pages/login/Login.vue
+++ src/views/pages/login/Login.vue
@@ -81,7 +81,7 @@ export default {
         username: '',
         password: ''
       },
-      title: GLOBAL_LOGINTITLE || (process.env && process.env.VUE_APP_LOGINTITLE),
+      title: (process.env && process.env.VUE_APP_LOGINTITLE) || GLOBAL_LOGINTITLE,
       errorTips: '',
       remember: false,
       loading: false,
