c836d6e3fa79cc8b29229dbd7c99bf53fd7c39d1
yaojn <yaojn@techsure.com.cn>
2025-01-14 10:13:47 +0800

    - [修复]添加定时作业，runner组标签添加为空判断

--- src/views/pages/autoexec/detail/runner-detail.vue
+++ src/views/pages/autoexec/detail/runner-detail.vue
@@ -667,8 +667,9 @@ export default {
       if (this.dataConfig.existRunnerOrSqlExecMode) {
         this.$set(data, 'runnerGroup', this.runnerGroup);
       }
-      if (this.$refs.runnerGroupTag) {
-        this.$set(this, 'runnerGroupTag', this.$refs.runnerGroupTag.save());
+      let runnerGroupTag = this.$refs.runnerGroupTag;
+      if (runnerGroupTag) {
+        this.$set(this, 'runnerGroupTag', runnerGroupTag.save());
       }
       this.$set(data, 'runnerGroupTag', this.runnerGroupTag);
       return data;
