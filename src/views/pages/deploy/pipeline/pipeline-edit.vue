7d61a453f87678c0db557fc90bc6144835d541c3
dengbeifen <dengbf@techsure.com.cn>
2025-04-19 14:42:23 +0800

    [修复] 超级流水线再次编辑作业报错

--- src/views/pages/deploy/pipeline/pipeline-edit.vue
+++ src/views/pages/deploy/pipeline/pipeline-edit.vue
@@ -289,7 +289,7 @@ export default {
       console.log(JSON.stringify(jobTemplateData, null, 2));
       if (!this.$utils.isEmpty(this.currentGroup)) {
         const index = this.currentGroup.jobTemplateList.findIndex(job => {
-          return job.id === jobTemplateData.id || job.uuid === jobTemplateData.uuid;
+          return (job.id && job.id === jobTemplateData.id) || (job.uuid && job.uuid === jobTemplateData.uuid);
         });
         if (index > -1) {
           this.$set(this.currentGroup.jobTemplateList, index, jobTemplateData);
