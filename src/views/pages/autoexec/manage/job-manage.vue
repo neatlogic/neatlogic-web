c8bda168184c270f207bc39959e7b83d331e8ada
1437892690@qq.com <1437892690@qq.com>
2025-04-21 15:23:39 +0800

    [修复] 作业管理-批量作业较多，展开收起异常
    关联 #[1404213169455104]作业管理-批量作业较多，展开收起异常 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1404213169455104

--- src/views/pages/autoexec/manage/job-manage.vue
+++ src/views/pages/autoexec/manage/job-manage.vue
@@ -313,6 +313,9 @@ export default {
             this.$set(parentRow, 'loading', false);
             this.jobData.tbodyList.splice(pIndex + 1, 0, ...jobList);
           }
+        } else {
+          this.$set(parentRow, 'showChildren', true);
+          this.$set(parentRow, 'loading', false);
         }
       });
     },
