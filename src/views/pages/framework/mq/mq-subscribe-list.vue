250b00269c95bb6daf64ba482e76226a9e5da984
chenqw <gdnpc@163.com>
2025-05-08 16:00:25 +0800

    -[修复]修复几个页面bug

--- src/views/pages/framework/mq/mq-subscribe-list.vue
+++ src/views/pages/framework/mq/mq-subscribe-list.vue
@@ -72,7 +72,7 @@ export default {
     return {
       isEditShow: false,
       currentSubscribeId: null,
-      searchParam: { ignoreServerId: true },
+      searchParam: { },
       subscribeData: {},
       theadList: [
         {
