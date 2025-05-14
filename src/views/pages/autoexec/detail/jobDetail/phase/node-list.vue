e80cf0571857caaa5cef8e080df9763e661d3c4f
lvzk <897706680@qq.com>
2025-05-14 21:40:51 +0800

    [功能]自动化发布sql增加"是否已更新"

--- src/views/pages/autoexec/detail/jobDetail/phase/node-list.vue
+++ src/views/pages/autoexec/detail/jobDetail/phase/node-list.vue
@@ -110,6 +110,9 @@
         <span v-if="row && row.extraInfo && row.extraInfo.version">{{ row.extraInfo.version }}</span>
         <span v-else>-</span>
       </template>
+      <template v-slot:isModified="{row}">
+        {{ row.isModified == 1?$t('page.yes'):$t('page.no') }}
+      </template>
       <template v-slot:blueGreenName="{ row }">
         <span v-if="row && row.extraInfo && row.extraInfo.blueGreenName">{{ row.extraInfo.blueGreenName }}({{ row.extraInfo.blueGreenSort }})</span>
         <span v-else>-</span>
@@ -354,7 +357,7 @@ export default {
   },
   beforeCreate() {},
   created() {
-    if (this.jobData.extraInfo && this.jobData.extraInfo.sourceType == 'deploy') {
+    if (this.jobData.extraInfo && this.jobData.extraInfo.sourceType == 'deploy' && this.phaseData.execMode != 'sqlfile') {
       // 添加发布版本字段
       this.theadList.splice(1, 0, {
         title: this.$t('page.versions'),
@@ -364,6 +367,13 @@ export default {
       this.theadList.splice(1, 0, {
         title: this.$t('term.deploy.blueSet'),
         key: 'blueGreenName'
+      });
+    }
+    if (this.phaseData.execMode == 'sqlfile') {
+      // 添加发布版本字段
+      this.theadList.splice(4, 0, {
+        title: '是否已更新',
+        key: 'isModified'
       });
     }
     if (this.jobData.isCanExecute) {
