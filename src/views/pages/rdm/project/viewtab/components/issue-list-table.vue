a2f7b285a398d6779c2e6a1816e418def6d5c18a
yaojn <yaojn@techsure.com.cn>
2025-06-11 18:50:25 +0800

    - [功能]子需求以连接方式打开   - [关联]#[1401685522481152]子需求以连接方式打开 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1401685522481152

--- src/views/pages/rdm/project/viewtab/components/issue-list-table.vue
+++ src/views/pages/rdm/project/viewtab/components/issue-list-table.vue
@@ -44,7 +44,7 @@
             @click="toggleChildIssue(row)"
           ></span>
           <span class="overflow">
-            <a href="javascript:void(0)" @click="openIssueDetail(row)" @contextmenu="newTab($event, row)">
+            <a href="javascript:void(0)" @click="openIssueDetail(row)">
               <span v-if="!issueData.wordList || issueData.wordList === 0">{{ row.name }}</span>
               <span v-else v-html="highlightKeywords(row.name, issueData.wordList)"></span>
             </a>
@@ -108,13 +108,10 @@ export default {
   beforeDestroy() {},
   destroyed() {},
   methods: {
-    newTab(e, row) {
-      //鼠标右键打开新标签页
-      const base = this.$router.options.base;
-      const { appType = '', projectId = null, appId = null, id = null, name = '' } = row || {};
-      const path = `/${appType}-detail/${projectId}/${appId}/${id}`;
-      const url = `<a href="${base}#${path}">${name}</a>`;
-      e.currentTarget.innerHTML = url;
+    newWindowOpen(row) {
+      const { appType = '', projectId = null, appId = null, id = null} = row || {};
+      const path = `${appType}-detail/${projectId}/${appId}/${id}`;
+      window.open(HOME + '/rdm.html#/' + path, '_blank');
     },
     highlightKeywords(text, wordList) {
       if (!wordList || wordList.length === 0) return text;
@@ -156,7 +153,12 @@ export default {
       this.$emit('deleteIssue', row);
     },
     openIssueDetail(row) {
-      this.$emit('openIssue', row);
+      if (row && row.parentId) {
+        // 子需求新标签页打开
+        this.newWindowOpen(row);
+      } else {
+        this.$emit('openIssue', row);
+      }
     },
     searchIssue(currentPage) {
       this.$emit('searchIssue', currentPage);
