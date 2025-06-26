14c59569bf2114f885ff41ca8addbf5357b7de5d
dengbf <dengbf@aliyun.com>
2025-06-26 11:15:54 +0800

    commit

--- src/views/pages/autoexec/detail/actionDetail/execute-setting.vue
+++ src/views/pages/autoexec/detail/actionDetail/execute-setting.vue
@@ -8,6 +8,7 @@
       <Loading :loadingShow="loadingShow" type="fix"></Loading>
       <div v-if="!loadingShow" class="pl-nm pr-nm">
         <div>
+          {{ settingConfig.parallelPolicy }}
           <TsForm
             ref="form"
             v-model="settingConfig"
@@ -183,19 +184,7 @@ export default {
           validateList: ['required'],
           transfer: true,
           onChange: (val) => {
-            if (val && val == 'roundCount') {
-              this.$set(this.form.roundCount, 'isHidden', false);
-              this.$set(this.form.parallelCount, 'isHidden', true);
-              this.$set(this.settingConfig, 'parallelCount', null);
-              this.$set(this.settingConfig, 'roundCount', 32);
-            } else {
-              this.$set(this.form.roundCount, 'isHidden', true);
-              this.$set(this.form.parallelCount, 'isHidden', false);
-              this.$set(this.settingConfig, 'parallelCount', 64);
-              this.$set(this.settingConfig, 'roundCount', null);
-            }
-            console.log(val);
-            console.log(this.executeConfig.parallelPolicy);
+            this.changeParallelPolicy(val);
           }
         },
         roundCount: {
@@ -385,6 +374,21 @@ export default {
     changeWhenToSpecify(val) {
       this.settingConfig.whenToSpecify = val;
       this.settingConfig.executeNodeConfig = {};
+    },
+    changeParallelPolicy(val) {
+      this.$nextTick(() => {
+        if (val && val == 'roundCount') {
+          this.$set(this.settingConfig, 'parallelCount', null);
+          this.$set(this.settingConfig, 'roundCount', 32);
+          this.$set(this.form.roundCount, 'isHidden', false);
+          this.$set(this.form.parallelCount, 'isHidden', true);
+        } else {
+          this.$set(this.settingConfig, 'parallelCount', 64);
+          this.$set(this.settingConfig, 'roundCount', null);
+          this.$set(this.form.roundCount, 'isHidden', true);
+          this.$set(this.form.parallelCount, 'isHidden', false);
+        }
+      });
     }
   },
   computed: {},
