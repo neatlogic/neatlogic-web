bcd8aed147f439546e1e1cd0d758fe1bdb6c10c7
chenqw <gdnpc@163.com>
2025-07-15 17:49:30 +0800

    adjust

--- src/views/pages/dashboard/widget/charts/original/bullet-widget.vue
+++ src/views/pages/dashboard/widget/charts/original/bullet-widget.vue
@@ -40,7 +40,7 @@ export default {
     createRandomData() {
       this.data = [];
       for (var i = 1; i <= 1; i++) {
-        this.data.push({ 
+        this.data.push({
           title: '完成度',
           ranges: [100],
           measures: [80],
@@ -68,7 +68,7 @@ export default {
   },
   filter: {},
   computed: {
-     finalData() {
+    finalData() {
       const data = [];
       if (this.data && this.data.length > 0) {
         //数据合并汇聚
