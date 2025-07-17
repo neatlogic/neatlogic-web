3e2926b8970bef08847d7b1e86343d3b14fa16a2
dengbf <dengbf@aliyun.com>
2025-07-17 11:00:30 +0800

    - [关联] #[1465989923831808]表单编辑-表格输入组件中，属性间的联动没有生效 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1465989923831808

--- src/resources/plugins/TsSheet/form/reaction/disable.js
+++ src/resources/plugins/TsSheet/form/reaction/disable.js
@@ -2,10 +2,10 @@ export default ({ overrideConfig, result, view }) => {
   if (!overrideConfig.isDisabled) {
     if (result) {
       view.$set(view.formItem.config, 'isDisabled', true);
-      view.currentItemDisabled = true;
+      view.$set(view.currentItemReaction, 'currentItemDisabled', true);
     } else {
       view.$set(view.formItem.config, 'isDisabled', false);
-      view.currentItemDisabled = false;
+      view.$set(view.currentItemReaction, 'currentItemDisabled', false);
     }
     view.addExecuteCount('disable');
   }
