86d5603b210b21e6b7e352ba18781d1134da353b
dengbf <dengbf@aliyun.com>
2025-10-14 18:08:20 +0800

    - [功能] 服务器推送

---
+++ src/resources/api/eventSource.js
@@ -0,0 +1,26 @@
+import { EventSourcePolyfill } from 'event-source-polyfill';
+
+const creatEventSourcePolyfill = (url, params) => {
+  let queryString = '';
+  if (params) {
+    queryString = '?' + Object.keys(params).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(params[key])).join('&');
+  }
+  const href = BASEURLPREFIX + url + queryString;
+  const es = new EventSourcePolyfill(href, {
+    heartbeatTimeout: 300000, //设置重连时间
+    headers: {
+      'source': 'pc',
+      'Content-Type': 'text/event-stream'
+    }
+  });
+  es.onopen = () => {
+    console.log('连接成功');
+  };
+  es.onerror = (err) => {
+    console.log('连接错误', err);
+  };
+  return es;
+};
+
+export default creatEventSourcePolyfill;
+
