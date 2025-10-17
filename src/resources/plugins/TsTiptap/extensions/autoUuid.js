d1ed6838b9558829991a08fa4acc699c21221ea7
yaojn <yaojn@techsure.com.cn>
2025-10-17 18:02:07 +0800

    - [功能]知识库重构     - [关联]#[1471108316889088]知识库重构 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1471108316889088

--- src/resources/plugins/TsTiptap/extensions/autoUuid.js
+++ src/resources/plugins/TsTiptap/extensions/autoUuid.js
@@ -4,6 +4,19 @@ import utils from '@/resources/assets/js/util.js';
 
 export const AutoUuid = Extension.create({
   name: 'autoUuid',
+  // 给所有 block 节点加全局属性
+  addGlobalAttributes() {
+    return [
+      {
+        types: ['paragraph', 'heading', 'todoItem', 'customBlock'], // 系统所有 block 类型
+        attributes: {
+          uuid: {
+            default: utils.setUuid()
+          }
+        }
+      }
+    ];
+  },
   addProseMirrorPlugins() {
     return [
       new Plugin({
@@ -12,9 +25,9 @@ export const AutoUuid = Extension.create({
           let modified = false;
           newState.doc.descendants((node, pos) => {
             // 只给块级节点加 uuid（比如 paragraph、heading、list_item）
-            if (!node.attrs.uuid) {
-              tr = tr.setNodeMarkup(pos, node.type, {
-                ...node.attrs,
+            if (node.type.isBlock && !node?.attrs?.uuid) {
+              tr = tr.setNodeMarkup(pos, node?.type, {
+                ...(node.attrs || {}),
                 uuid: utils.setUuid()
               });
               modified = true;
