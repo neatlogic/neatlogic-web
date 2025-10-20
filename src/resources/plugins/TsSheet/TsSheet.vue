5865673dcea8e019cbdff4eb5e04c816fc8da5ed
yaojn <yaojn@techsure.com.cn>
2025-10-20 16:09:31 +0800

    - [修复]表单设置行联动规则保存失效    - [关联]#[1536285267689472]表单设置行联动规则保存失效 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1536285267689472

--- src/resources/plugins/TsSheet/TsSheet.vue
+++ src/resources/plugins/TsSheet/TsSheet.vue
@@ -1066,6 +1066,15 @@ export default {
     },
     clearReactionRow(reaction) { //清除行联动的废数据
       if (!this.$utils.isEmpty(reaction)) {
+        let tableRowList = [];
+        (this.config?.tableList || []).forEach((v) => {
+          if (v && v.hasOwnProperty('row')) {
+            const currentRow = v.row + 1;
+            if (!tableRowList.includes(currentRow)) {
+              tableRowList.push(currentRow);
+            }
+          }
+        });
         Object.keys(reaction).forEach(key => {
           if (!this.$utils.isEmpty(reaction[key])) {
             reaction[key].forEach(item => {
@@ -1087,11 +1096,17 @@ export default {
                 item.conditionGroupList = item.conditionGroupList.filter(c => !groupUuidList.includes(c.uuid));
               }
               if (this.$utils.isEmpty(item.conditionGroupList)) {
-                item.rows = [];
+                if (!this.$utils.isEmpty(item.rows)) {
+                  for (let j = item.rows.length - 1; j >= 0; j--) {
+                    if (!tableRowList.includes(j)) {
+                      item.rows.splice(j, 1);
+                    }
+                  }
+                }
                 item.conditionGroupRelList = [];
               }
             });
-            reaction[key] = reaction[key].filter(item => !this.$utils.isEmpty(item.conditionGroupList));
+            reaction[key] = reaction[key].filter(item => !this.$utils.isEmpty(item.rows));
           }
         });
         //更新配置
