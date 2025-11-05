8111518217a26fc3b70c6e02adb6f7954f2b0326
chenqw <gdnpc@163.com>
2025-11-05 11:35:03 +0800

    adjust

---
+++ src/resources/components/Condition/components/json-condition-handler.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <TsFormInput
+      border="border"
+      :value="value"
+      v-bind="$attrs"
+      @on-change="changeValue"
+    ></TsFormInput>
+  </div>
+</template>
+<script>
+import { ConditionHandlerBase } from '@/resources/components/Condition/components/base-condition-handler.js';
+
+export default {
+  name: '',
+  components: {
+    TsFormInput: () => import('@/resources/plugins/TsForm/TsFormInput')
+  },
+  extends: ConditionHandlerBase,
+  inheritAttrs: false,
+  props: {},
+  data() {
+    return {};
+  },
+  beforeCreate() {},
+  async created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  activated() {},
+  deactivated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {},
+  filter: {},
+  computed: {},
+  watch: {}
+};
+</script>
+<style lang="less"></style>
