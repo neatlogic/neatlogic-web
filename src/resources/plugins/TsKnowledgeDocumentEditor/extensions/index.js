f5a005e475dca2c2e7a6216583214dcf933cbd08
yaojn <yaojn@techsure.com.cn>
2025-12-08 18:43:36 +0800

    - [功能]知识库重构   - [关联]#[1471108316889088]知识库重构 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1471108316889088

--- src/resources/plugins/TsKnowledgeDocumentEditor/extensions/index.js
+++ src/resources/plugins/TsKnowledgeDocumentEditor/extensions/index.js
@@ -1,10 +1,8 @@
 import InsertVideo from './insert-video';
 import HighlightBlock from './highlight-block';
 import { BlockUuid } from './block-uuid';
-import HeadingExtend from './extend/heading';
 export default [
   InsertVideo,
   HighlightBlock,
-  BlockUuid,
-  HeadingExtend
+  BlockUuid
 ];
