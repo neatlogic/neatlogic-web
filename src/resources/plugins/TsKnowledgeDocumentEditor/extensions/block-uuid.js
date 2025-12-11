4634b8f37936916700f3a0cebf82a1c5f734b7c9
yaojn <yaojn@techsure.com.cn>
2025-12-11 12:15:44 +0800

    - [功能]处理块级元素添加uuid属性(知识库重构)   - [关联]#[1471108316889088]知识库重构 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1471108316889088

--- src/resources/plugins/TsKnowledgeDocumentEditor/extensions/block-uuid.js
+++ src/resources/plugins/TsKnowledgeDocumentEditor/extensions/block-uuid.js
@@ -4,38 +4,75 @@ import utils from '@/resources/assets/js/util.js';
 
 const BlockUuid = Extension.create({
   name: 'blockUuid',
-  // 给所有 block 节点加全局属性
   addGlobalAttributes() {
     return [
       {
-        types: ['*'],
+        types: [
+          'paragraph',
+          'heading',
+          'blockquote',
+          'bulletList',
+          'orderedList',
+          'listItem',
+          'taskList',
+          'taskItem',
+          'codeBlock',
+          'horizontalRule',
+          'table',
+          'image',
+          'insertVideo',
+          'highlightBlock'
+        ],
         attributes: {
           'data-uuid': {
-            default: utils.setUuid(),
+            default: null, // 不要默认值，避免重复 uuid
             parseHTML: element => element.getAttribute('data-uuid'),
             renderHTML: attributes => {
+              if (!attributes['data-uuid']) return {};
               return { 'data-uuid': attributes['data-uuid'] };
+            }
+          },
+          'data-block-type': {
+            default: null,
+            parseHTML: element => element.getAttribute('data-block-type'),
+            renderHTML: attributes => {
+              if (!attributes['data-block-type']) return {};
+              return { 'data-block-type': attributes['data-block-type'] };
             }
           }
         }
       }
     ];
   },
+
+  /**
+   * 只处理“新增的 block 节点”
+   * 避免每次扫描全文档，提高性能
+   */
   addProseMirrorPlugins() {
     return [
       new Plugin({
-        // appendTransaction 在节点内容变更或者插入内容的时候触发
         appendTransaction: (transactions, oldState, newState) => {
+          // 如果没有内容变动，不处理
+          const docChanged = transactions.some(tr => tr.docChanged);
+          if (!docChanged) return null;
+
           let tr = newState.tr;
           let modified = false;
+
           newState.doc.descendants((node, pos) => {
-            if (!node?.attrs?.['data-uuid']) {
-              tr = tr.setNodeMarkup(pos, node?.type, {
-                ...(node.attrs || {}),
-                'data-uuid': utils.setUuid()
-              });
-              modified = true;
-            }
+            // 只处理 block 节点
+            if (!node.type.isBlock) return;
+
+            // 已有 uuid 的跳过
+            if (node.attrs['data-uuid']) return;
+
+            tr = tr.setNodeMarkup(pos, node.type, {
+              ...node.attrs,
+              'data-uuid': utils.setUuid(),
+              'data-block-type': node.type.name == 'heading' ? `heading${node.attrs.level}` : node.type.name
+            });
+            modified = true;
           });
           return modified ? tr : null;
         }
