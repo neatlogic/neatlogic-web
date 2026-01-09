6b0135b36db7470a215faf1a300a0523ed06bdad
linbangquan <1437892690@qq.com>
2026-01-09 01:39:45 +0000

    !1314 [功能] 系统参数管理页面增加查看数据库、mongodb、ES等配置信息 * Merge branch 'develop4.0.0' into branch-1592042113695748 * [功能] 系统参数管理页面增加查看数据库、mongodb、ES等配置信息 * [功能] 系统参数管理页面增加查看数据库、mongodb、ES等配置信息

---
+++ src/views/pages/framework/tenantconfig/parametersettings-manage.vue
@@ -0,0 +1,156 @@
+<template>
+  <div>
+    <TsContain :hideHeader="true">
+      <template v-slot:content>
+        <TsTable
+          v-if="!loading"
+          v-bind="tableData"
+          :theadList="theadList"
+          @changeCurrent="changePage"
+          @changePageSize="changePageSize"
+        >
+          <template v-slot:key="{ row }">
+            <span class="text-href" @click.stop="showTenantConfigForm(row.key)">{{ row.key }}</span>
+          </template>
+        </TsTable>
+      </template>
+    </TsContain>
+    <TsDialog
+      v-if="isShowFormDialog"
+      type="modal"
+      :isShow="true"
+      :title="$t('dialog.title.edittarget', { target: $t('page.param') })"
+      @on-close="close"
+      @on-ok="save"
+    >
+      <template v-slot>
+        <TsForm
+          ref="mainForm"
+          v-model="rowData"
+          :itemList="formSetting"
+          type="type"
+          labelPosition="right"
+        ></TsForm>
+      </template>
+    </TsDialog>
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  components: {
+    TsForm: () => import('@/resources/plugins/TsForm/TsForm'),
+    TsTable: () => import('@/resources/components/TsTable/TsTable.vue')
+  },
+  props: {},
+  data() {
+    return {
+      loading: false,
+      currentPage: 1,
+      pageSize: 20,
+      theadList: [
+        {
+          key: 'key',
+          title: this.$t('page.variable')
+        },
+        {
+          key: 'value',
+          title: this.$t('page.value'),
+          maxLength: 200
+        },
+        {
+          key: 'description',
+          title: this.$t('page.description'),
+          maxLength: 200
+        }
+      ],
+      tableData: {},
+      isShowFormDialog: false,
+      formSetting: {
+        key: {
+          type: 'text',
+          label: this.$t('page.variable'),
+          disabled: true,
+          validateList: ['required']
+        },
+        value: {
+          type: 'text',
+          label: this.$t('page.value'),
+          validateList: ['required']
+        },
+        description: {
+          type: 'textarea',
+          label: this.$t('page.description'),
+          disabled: true
+        }
+      },
+      rowData: {}
+    };
+  },
+  beforeCreate() {},
+  created() {
+    this.listTenantConfig();
+  },
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  activated() {},
+  deactivated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {
+    changePage(currentPage) {
+      this.currentPage = currentPage;
+      this.listTenantConfig();
+    },
+    changePageSize(pageSize) {
+      this.currentPage = 1;
+      this.pageSize = pageSize;
+      this.listTenantConfig();
+    },
+    listTenantConfig() {
+      this.loading = true;
+      this.$api.framework.tenantconfig
+        .listTenantConfig({ currentPage: this.currentPage, pageSize: this.pageSize })
+        .then(res => {
+          if (res.Status === 'OK') {
+            this.tableData = res.Return;
+          }
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    async showTenantConfigForm(key) {
+      await this.$api.framework.tenantconfig.getTenantConfig({ key }).then(res => {
+        if (res.Status === 'OK') {
+          this.rowData = res.Return;
+        }
+      });
+      this.isShowFormDialog = true;
+    },
+    close() {
+      this.isShowFormDialog = false;
+      this.rowData = {};
+    },
+    save() {
+      let form = this.$refs.mainForm;
+      if (form.valid()) {
+        let data = form.getFormValue();
+        this.$api.framework.tenantconfig.saveTenantConfig(data).then(res => {
+          if (res.Status === 'OK') {
+            this.isShowFormDialog = false;
+            this.rowData = {};
+            this.listTenantConfig();
+          }
+        });
+      }
+    }
+  },
+  filter: {},
+  computed: {},
+  watch: {}
+};
+</script>
+<style lang="less"></style>
