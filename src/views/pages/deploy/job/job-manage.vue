a24da4c198a4e7f69ecb238554f6e942e82d2388
lvzk <897706680@qq.com>
2026-01-29 15:46:45 +0800

    [修复]一键发布-选择模块后，无法添加作业，添加按钮提示文案为空 #[1609343139545093]一键发布-选择模块后，无法添加作业，添加按钮提示文案为空 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1609343139545093

--- src/views/pages/deploy/job/job-manage.vue
+++ src/views/pages/deploy/job/job-manage.vue
@@ -18,17 +18,17 @@
                   <ul slot="content">
                     <li v-if="!selectedApp.isConfig"><span>{{ $t('term.deploy.currentapplynoconfig') }}</span>,<span v-if="canEditAuth" class="text-href" @click="toPipeline()">{{ $t('dialog.title.addtarget', {target: $t('page.config')}) }}</span><span v-else>{{ $t('page.deploy.contactwithadmin',{target: $t('dialog.title.addtarget', {target: $t('page.config')})}) }}</span></li>
                     <li v-else-if="!selectedApp.isHasModule"><span>{{ $t('term.deploy.applynoconfigmodule') }}</span>,<span v-if="canEditAuth" class="text-href" @click="addModule()">{{ $t('dialog.title.addtarget', {target: $t('page.module')}) }}</span><span v-else>{{ $t('page.deploy.contactwithadmin',{target: $t('dialog.title.addtarget', {target: $t('page.module')})}) }}</span></li>
+                    <li v-else-if="selectedModule && !selectedModule.isHasEnv">
+                      <span>{{ selectedApp.abbrName }}/{{ selectedModule.abbrName }}{{ selectedModule.name?'['+selectedModule.name+']':'' }}{{ $t('term.deploy.noconfigenv') }}</span>,
+                      <span v-if="canEditAuth" class="text-href" @click="addEnv(selectedModule.id)">{{ $t('dialog.title.addtarget', {target: $t('page.environment')}) }}</span>
+                      <span v-else>{{ $t('page.deploy.contactwithadmin',{target: $t('dialog.title.addtarget', {target: $t('page.environment')})}) }}</span>
+                    </li>
                     <li v-else-if="selectedApp.isHasModule && !selectedApp.isHasEnv">
                       <div v-for="item in moduleList" :key="item.id" class="pb-sm">
                         <span>{{ selectedApp.abbrName }}/{{ item.abbrName }}{{ item.name?'['+item.name+']':'' }}{{ $t('term.deploy.noconfigenv') }}</span>,
                         <span v-if="canEditAuth" class="text-href" @click="addEnv(item.id)">{{ $t('dialog.title.addtarget', {target: $t('page.environment')}) }}</span>
                         <span v-else>{{ $t('page.deploy.contactwithadmin',{target: $t('dialog.title.addtarget', {target: $t('page.environment')})}) }}</span>
                       </div>
-                    </li>
-                    <li v-else-if="selectedModule && !selectedModule.isHasEnv">
-                      <span>{{ selectedApp.abbrName }}/{{ selectedModule.abbrName }}{{ selectedModule.name?'['+selectedModule.name+']':'' }}{{ $t('term.deploy.noconfigenv') }}</span>,
-                      <span v-if="canEditAuth" class="text-href" @click="addEnv(selectedModule.id)">{{ $t('dialog.title.addtarget', {target: $t('page.environment')}) }}</span>
-                      <span v-else>{{ $t('page.deploy.contactwithadmin',{target: $t('dialog.title.addtarget', {target: $t('page.environment')})}) }}</span>
                     </li>
                     <li v-else-if="!hasScenarioAuth">
                       <div><span>{{ $t('term.deploy.noconfigscenauth') }}</span>,<span v-if="canEditAuth" class="text-href" @click="openAuthDialog">{{ $t('dialog.title.addtarget', {target: $t('page.auth')}) }}</span><span v-else>{{ $t('page.deploy.contactwithadmin',{target: $t('page.auth')}) }}</span></div>
@@ -346,8 +346,8 @@ export default {
       const {isConfig, isHasModule} = this.selectedApp;
       const isHasEnv = this.selectedModule ? this.selectedModule.isHasEnv : this.selectedApp.isHasEnv;
       //if (this.canEditAuth && !this.hasEnvAuth && (isConfig && isHasModule && !isHasEnv)) {
-        await this.getModuleList(this.selectedApp);
-       // return false;
+      await this.getModuleList(this.selectedApp);
+      // return false;
       // }
     },
     // toOperationDetail(row) {
