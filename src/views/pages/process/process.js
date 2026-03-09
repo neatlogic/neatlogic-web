ec098dd0f2025fad7c337f8f793a2742461ed432
yaojn <yaojn@techsure.com.cn>
2026-03-09 14:22:37 +0800

    - [功能]优化路由管理：模块路由跨模块导入，统一注册入口，无需在各模块 router.js 中手动添加。      - [关联]#[1634731293409284]优化路由管理：模块路由跨模块导入，统一注册入口，无需在各模块 router.js 中手动添加。 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1634731293409284

--- src/views/pages/process/process.js
+++ src/views/pages/process/process.js
@@ -10,6 +10,7 @@ import api from '@/resources/api/api.js';
 import LocalStore from '@/resources/assets/js/localStore.js';
 import State from '@/resources/assets/js/state.js';
 import commonRouters from '@/views/pages/common/router.js';
+import { buildMergedRoutes } from '@/resources/import/router-config.js';
 
 //公共的全局组件、样式等
 import {initRouter, initI18n} from '@/resources/init.js';
@@ -19,7 +20,7 @@ Vue.use(VueI18n);
 Vue.use(CompareUtil);//必须要在use router之后执行
 import {config} from './config.js';
 MODULEID = config.module;
-MENULIST = [...commonRouters, ...routers];
+MENULIST = buildMergedRoutes(MODULEID, routers, commonRouters);
 MENUTYPE = config.menuType;
 
 let router = initRouter(VueRouter, store);//路由拦截处理
--- src/views/pages/report/report.js
+++ src/views/pages/report/report.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import '@/resources/base.js';
 import Report from './report.vue';
 import routers from './router.js';
 import store from '@/resources/store';
@@ -9,11 +10,11 @@ import api from '@/resources/api/api.js';
 import LocalStore from '@/resources/assets/js/localStore.js';
 import State from '@/resources/assets/js/state.js';
 import commonRouters from '@/views/pages/common/router.js';
+import { buildMergedRoutes } from '@/resources/import/router-config.js';
 import { config } from './config.js';
 import md5 from 'js-md5';
 
 //公共的全局组件、样式等
-import '@/resources/base.js';
 import { initRouter, initI18n } from '@/resources/init.js';
 
 Vue.use(VueRouter);
@@ -21,7 +22,7 @@ Vue.use(VueI18n);
 Vue.use(CompareUtil); //必须要在use router之后执行
 
 MODULEID = config.module;
-MENULIST = [...commonRouters, ...routers];
+MENULIST = buildMergedRoutes(MODULEID, routers, commonRouters);
 MENUTYPE = config.menuType;
 
 let router = initRouter(VueRouter, store); //路由拦截处理
