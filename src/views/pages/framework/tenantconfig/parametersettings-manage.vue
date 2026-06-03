8672c28c6410e6ee64b78ae873c751dbbe3a4ca7
lvzk <897706680@qq.com>
2026-06-03 11:12:41 +0800

    [功能]优化系统配置根据模块组分类

--- src/views/pages/framework/tenantconfig/parametersettings-manage.vue
+++ src/views/pages/framework/tenantconfig/parametersettings-manage.vue
@@ -1,19 +1,37 @@
 <template>
   <div>
-    <TsContain>
+    <TsContain :siderWidth="240" enableCollapse>
       <template v-slot:topRight>
         <div>
           <Row span="24" :gutter="16">
             <Col span="12"></Col>
             <Col span="12">
-              <InputSearcher v-model="keyword" @change="changePage(1)"></InputSearcher>
+              <InputSearcher v-model="keyword" @change="searchTenantConfig"></InputSearcher>
             </Col>
           </Row>
         </div>
       </template>
+      <template v-slot:sider>
+        <div class="tenantconfig-module-index">
+          <div
+            v-for="moduleGroup in moduleGroupList"
+            :key="moduleGroup.moduleGroup"
+            class="module-index-item text-action"
+            :class="{ 'is-active': selectedModuleGroup === moduleGroup.moduleGroup }"
+            @click="changeModuleGroup(moduleGroup.moduleGroup)"
+          >
+            <div class="module-index-name overflow" :title="moduleGroup.moduleGroupName || moduleGroup.moduleGroup">
+              {{ moduleGroup.moduleGroupName || moduleGroup.moduleGroup }}
+            </div>
+            <div class="module-index-meta text-grey overflow" :title="moduleGroup.moduleGroup">
+              {{ moduleGroup.moduleGroup }} · {{ moduleGroup.configCount || 0 }} {{ $t('page.strip') }}
+            </div>
+          </div>
+          <NoData v-if="!moduleLoading && moduleGroupList.length === 0"></NoData>
+        </div>
+      </template>
       <template v-slot:content>
         <TsTable
-          v-if="!loading"
           v-bind="tableData"
           :theadList="theadList"
           @changeCurrent="changePage"
@@ -51,15 +69,19 @@ export default {
   components: {
     TsForm: () => import('@/resources/plugins/TsForm/TsForm'),
     InputSearcher: () => import('@/resources/components/InputSearcher/InputSearcher.vue'),
+    NoData: () => import('@/resources/components/nodata/NoData.vue'),
     TsTable: () => import('@/resources/components/TsTable/TsTable.vue')
   },
   props: {},
   data() {
     return {
       loading: false,
+      moduleLoading: false,
       keyword: null,
       currentPage: 1,
       pageSize: 20,
+      moduleGroupList: [],
+      selectedModuleGroup: null,
       theadList: [
         {
           key: 'key',
@@ -101,7 +123,7 @@ export default {
   },
   beforeCreate() {},
   created() {
-    this.listTenantConfig();
+    this.refreshTenantConfig();
   },
   beforeMount() {},
   mounted() {},
@@ -121,10 +143,27 @@ export default {
       this.pageSize = pageSize;
       this.listTenantConfig();
     },
+    searchTenantConfig() {
+      this.currentPage = 1;
+      this.refreshTenantConfig();
+    },
+    refreshTenantConfig() {
+      this.listTenantConfigModuleGroup().then(() => {
+        this.listTenantConfig();
+      });
+    },
     listTenantConfig() {
       this.loading = true;
+      const params = {
+        currentPage: this.currentPage,
+        pageSize: this.pageSize,
+        keyword: this.keyword
+      };
+      if (this.selectedModuleGroup) {
+        params.moduleGroup = this.selectedModuleGroup;
+      }
       this.$api.framework.tenantconfig
-        .listTenantConfig({ currentPage: this.currentPage, pageSize: this.pageSize, keyword: this.keyword })
+        .listTenantConfig(params)
         .then(res => {
           if (res.Status === 'OK') {
             this.tableData = res.Return;
@@ -133,6 +172,27 @@ export default {
         .finally(() => {
           this.loading = false;
         });
+    },
+    listTenantConfigModuleGroup() {
+      this.moduleLoading = true;
+      return this.$api.framework.tenantconfig
+        .listTenantConfig({ keyword: this.keyword, isGroupByModule: 1 })
+        .then(res => {
+          if (res.Status === 'OK') {
+            this.moduleGroupList = (res.Return && res.Return.tbodyList) || [];
+            if (this.selectedModuleGroup && !this.moduleGroupList.find(moduleGroup => moduleGroup.moduleGroup === this.selectedModuleGroup)) {
+              this.selectedModuleGroup = null;
+            }
+          }
+        })
+        .finally(() => {
+          this.moduleLoading = false;
+        });
+    },
+    changeModuleGroup(moduleGroup) {
+      this.selectedModuleGroup = this.selectedModuleGroup === moduleGroup ? null : moduleGroup;
+      this.currentPage = 1;
+      this.listTenantConfig();
     },
     async showTenantConfigForm(key) {
       await this.$api.framework.tenantconfig.getTenantConfig({ key }).then(res => {
@@ -163,7 +223,7 @@ export default {
           if (res.Status === 'OK') {
             this.isShowFormDialog = false;
             this.rowData = {};
-            this.listTenantConfig();
+            this.refreshTenantConfig();
           }
         });
       }
@@ -174,4 +234,38 @@ export default {
   watch: {}
 };
 </script>
-<style lang="less"></style>
+<style lang="less" scoped>
+.tenantconfig-module-index {
+  height: 100%;
+  padding: 8px 0;
+
+  .module-index-item {
+    padding: 8px 12px;
+    line-height: 18px;
+    cursor: pointer;
+
+    &:hover {
+      background: rgba(0, 0, 0, 0.04);
+    }
+
+    &.is-active {
+      border-left: 3px solid var(--primary-color);
+      background: rgba(0, 0, 0, 0.04);
+      padding-left: 9px;
+
+      .module-index-name {
+        color: var(--primary-color);
+      }
+    }
+  }
+
+  .module-index-name {
+    font-weight: 500;
+  }
+
+  .module-index-meta {
+    margin-top: 2px;
+    font-size: 12px;
+  }
+}
+</style>
