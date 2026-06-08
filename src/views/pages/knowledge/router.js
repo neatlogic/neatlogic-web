2230ceccc11b9d9dfe0bee16e03281a29e768528
1437892690@qq.com <1437892690@qq.com>
2026-06-08 15:57:02 +0800

    [功能] 飞书文档导入 neatlogic 知识库
    关联 #[1695041165459459]飞书文档导入 neatlogic 知识库 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1695041165459459

--- src/views/pages/knowledge/router.js
+++ src/views/pages/knowledge/router.js
@@ -7,7 +7,7 @@ const circleManage = () => import('./circle/circle-manage.vue');
 const circleEdit = () => import('./circle/circle-edit.vue');
 const templateManage = () => import('./template/template-manage.vue');
 const templateEdit = () => import('./template/template-edit.vue');
-const feishuSyncManage = () => import('./feishu/feishu-sync-manage.vue');
+const dataSynchronizationManage = () => import('./synchronization/data-synchronization-manage.vue');
 const reviewDetail = () => import('./category/review/review-detail.vue');
 const KnowledgeOverview = () => import('./category/category/knowledge-overview.vue');
 const KnowledgeSearch = () => import('./category/category/knowledge-search.vue');
@@ -122,11 +122,11 @@ export default [
     }
   },
   {
-    path: '/feishu-sync-manage',
-    name: 'feishu-sync-manage',
-    component: feishuSyncManage,
+    path: '/data-synchronization-manage',
+    name: 'data-synchronization-manage',
+    component: dataSynchronizationManage,
     meta: {
-      title: '同步飞书知识库',
+      title: $t('router.knowledge.synchronizationmanage'),
       ismenu: true,
       icon: 'tsfont-sync',
       authority: 'KNOWLEDGE_FEISHU_SYNC_MODIFY',
---
+++ src/views/pages/knowledge/synchronization/data-synchronization-manage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="mt-md">
+    <TsContain hideHeader>
+      <template v-slot:content>
+        <Tabs v-model="currentTab" :animated="false">
+          <TabPane :label="$t('term.knowledge.feishu')" name="feishu"></TabPane>
+        </Tabs>
+        <div>
+          <div v-if="currentTab == 'feishu'">
+            <FeishuSyncManage></FeishuSyncManage>
+          </div>
+        </div>
+      </template>
+    </TsContain>
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  components: {
+    FeishuSyncManage: () => import('./feishu/feishu-sync-manage.vue')
+  },
+  props: {},
+  data() {
+    return {
+      currentTab: 'feishu'
+    };
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  activated() {},
+  deactivated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {},
+  filter: {},
+  computed: {},
+  watch: {}
+};
+</script>
+<style lang="less" scoped>
+</style>
