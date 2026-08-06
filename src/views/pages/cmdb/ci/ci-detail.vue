5158abaa5c178b66f9d0137d945b02e0df4d259e
dengbf <dengbf@aliyun.com>
2026-08-06 14:25:10 +0800

    - [修复] 英文模式，顶部栏超出范围

--- src/views/pages/cmdb/ci/ci-detail.vue
+++ src/views/pages/cmdb/ci/ci-detail.vue
@@ -4,7 +4,7 @@
       <template v-slot:navigation>
         <span v-if="$hasBack()" class="tsfont-left text-action" @click="$back()">{{ $getFromPage() }}</span>
       </template>
-      <template v-slot:topRight>
+      <template v-slot:top>
         <div v-if="ciData.authData && ciData.authData['cimanage']" class="dashboard-action action-group" style="text-align: right">
           <span class="action-item tsfont-edit" @click="editCi()">{{ $t('page.basicinfo') }}</span>
           <span v-if="!ciData.isVirtual" class="action-item tsfont-plus" @click="addAttr()">{{ $t('page.attribute') }}</span>
